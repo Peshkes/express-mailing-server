0c6bf10b28d45bc8f0394b2c63c56b0ab92d4dda
peshkes <peshkesv@gmail.com>
2024-08-27 16:08:47 +0400

    Init project

---
+++ api/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+
+const server = express();
+
+server.use(cors());
+server.use(helmet());
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+
+
+module.exports = server;
